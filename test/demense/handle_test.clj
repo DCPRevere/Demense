9c983bc6b3bad1d6230fb0b2253c6d787eb53e9c
Daniel Revere <dcprevere@gmail.com>
2017-02-13 19:12:25 +0000

    Create empty handle test.

---
+++ test/demense/handle_test.clj
@@ -0,0 +1,8 @@
+(ns demense.handle-test
+  (:require [demense.handle :as sut]
+            [clojure.test :as t]))
+
+;; Won't testing the handler just replicate any test we perform
+;; on the domain?
+
+;; (t/deftest "Creating item returns correct")
