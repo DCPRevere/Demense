9312afa5fbc890c9df34bc28239b9bacb7725d46
Daniel Revere <dcprevere@gmail.com>
2017-02-16 19:59:59 +0000

    Disable rest calls that rely on removed records.

--- src/demense/rest.clj
+++ src/demense/rest.clj
@@ -11,27 +11,24 @@
                      wrap-json-response]]
             [demense.repository :as repo]))
 
-(def r (repo/->EventStoreRepo))
+;; (def r (repo/->EventStoreRepo))
 
 (defroutes bare-handler
   ;; (GET "/search/:id" [id]
   ;;      (response (str "You asked for: " id))) ;; (:item/activated? (repo/get-by-id r id))))
-  (GET "/search/:id" [id]
-       (response (repo/get-by-id r id)))
-  (GET "/create/:id" [id name]
-       (response (h/handle (h/->CreateInventoryItem id name))))
-  (GET "/deactivate/:id" [id]
-       (response (h/handle (h/->DeactivateInventoryItem id))))
-  (GET "/remove/:id" [id count]
-       (response (h/handle (h/->RemoveItemsFromInventory id count))))
-  (GET "/checkin/:id" [id count]
-       (response (h/handle (h/->CheckInItemsToInventory id count))))
-  (GET "/rename/:id" [id name]
-       (response (h/handle (h/->RenameInventoryItem id name)))))
-
-(repo/get-by-id (repo/->EventStoreRepo) 3)
-
-(h/handle (h/->CreateInventoryItem 4 "tea"))
+  ;; (GET "/search/:id" [id]
+  ;;      (response (repo/get-by-id r id)))
+  ;; (GET "/create/:id" [id name]
+  ;;      (response (h/handle (h/->CreateInventoryItem id name))))
+  ;; (GET "/deactivate/:id" [id]
+  ;;      (response (h/handle (h/->DeactivateInventoryItem id))))
+  ;; (GET "/remove/:id" [id count]
+  ;;      (response (h/handle (h/->RemoveItemsFromInventory id count))))
+  ;; (GET "/checkin/:id" [id count]
+  ;;      (response (h/handle (h/->CheckInItemsToInventory id count))))
+  ;; (GET "/rename/:id" [id name]
+  ;;      (response (h/handle (h/->RenameInventoryItem id name))))
+  )
 
 (def handler
   (-> bare-handler
@@ -39,8 +36,7 @@
       wrap-json-params
       wrap-json-response))
 
-(defn start
-  []
+(defn start []
   (jetty/run-jetty handler {:port 8080}))
 
 
