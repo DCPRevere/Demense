042d6be87ee019df19132adb588ae1bd905851d2
Daniel Revere <dcprevere@gmail.com>
2017-02-16 19:58:07 +0000

    Start repostiry tests.

--- src/demense/specs/repository.clj
+++ src/demense/specs/repository.clj
@@ -1,3 +1,13 @@
 (ns demense.specs.repository
   (:require [clojure.spec :as s]
-            [clojure.spec.test :as s.t]))
+            [clojure.spec.test :as s.t]
+            [demense.repository :as repo]))
+
+(s/fdef repo/remove-prefix
+        :args (s/cat :str string? :prefix string?)
+        :ret string?
+        :fn (fn [{:keys [args ret]}]
+              (or (= ret (:str args))
+                  (= (:str args) (str (:prefix args) ret)))))
+
+(s.t/check `repo/remove-prefix)
